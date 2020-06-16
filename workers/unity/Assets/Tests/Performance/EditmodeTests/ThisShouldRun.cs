924b82b2254ed55cd73595fe4f6f7e1866e39c73
Paul Balaji <paulbalaji@improbable.io>
2020-06-16 12:44:53 +0100

    prune redundant tests
    Signed-off-by: Paul Balaji <paulbalaji@improbable.io>

--- workers/unity/Assets/Tests/Performance/EditmodeTests/ThisShouldRun.cs
+++ workers/unity/Assets/Tests/Performance/EditmodeTests/ThisShouldRun.cs
@@ -6,55 +6,7 @@ namespace Improbable.Gdk.EditmodePerformanceTests
     public class ThisShouldRun
     {
         [Performance, Test]
-        public void Mono_Default()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [Performance, Test]
-        public void Mono_Disabled()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [Performance, Test]
-        public void Mono_Any()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [Performance, Test]
-        public void Il2Cpp_Default()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [Performance, Test]
-        public void Il2Cpp_Disabled()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [Performance, Test]
-        public void Il2Cpp_Any()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [Performance, Test]
-        public void Any_Default()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [Performance, Test]
-        public void Any_Disabled()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [Performance, Test]
-        public void Any_Any()
+        public void SampleEditmodePerformanceTest()
         {
             Assert.IsTrue(true);
         }
