a0f05781c9bbd540f3eb4518000d1738f10b007a
Paul Balaji <paulbalaji@improbable.io>
2020-06-24 16:41:03 +0100

    try no editmode

--- ci/perf-test.sh
+++ ci/perf-test.sh
@@ -55,17 +55,17 @@ function runTests {
     popd
 }
 
-traceStart "Performance Testing: Editmode :writing_hand:"
-    for burst in burst-default burst-disabled
-    do
-        for apiProfile in dotnet-std-2 dotnet-4
-        do
-            traceStart "${burst} ${apiProfile}"
-                runTests "editmode" "Performance" ${burst} ${apiProfile}
-            traceEnd
-        done
-    done
-traceEnd
+# traceStart "Performance Testing: Editmode :writing_hand:"
+#     for burst in burst-default burst-disabled
+#     do
+#         for apiProfile in dotnet-std-2 dotnet-4
+#         do
+#             traceStart "${burst} ${apiProfile}"
+#                 runTests "editmode" "Performance" ${burst} ${apiProfile}
+#             traceEnd
+#         done
+#     done
+# traceEnd
 
 traceStart "Performance Testing: Playmode :joystick:"
     for burst in burst-default burst-disabled
